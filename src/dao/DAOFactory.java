45b6cf7d262ac9a2ca663a78de26ba677a4f2f3e
SanoP <sanodeemantha@gmail.com>
2020-07-30 16:51:00 +0530

    Update DAOFactory

--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -8,7 +8,6 @@ import dao.custom.impl.CustomerDAOImpl;
 import dao.custom.impl.ItemDAOImpl;
 import dao.custom.impl.OrderDAOImpl;
 import dao.custom.impl.OrderDetailDAOImpl;
-import entity.OrderDetail;
 
 public class DAOFactory {
     private static DAOFactory daoFactory;
@@ -25,15 +24,31 @@ public class DAOFactory {
         return new CustomerDAOImpl();
     }
 
-    public ItemDAO getItemDAO(){
-        return new ItemDAOImpl();
+    public SuperDAO getDAO(int daoType){
+
+        switch (daoType){
+            case 1:
+                return new CustomerDAOImpl();
+            case 2:
+                return new ItemDAOImpl();
+            case 3:
+                return new OrderDAOImpl();
+            case 4:
+                return new OrderDetailDAOImpl();
+            default:
+                return null;
+        }
     }
 
-    public OrderDAO getOrderDAO(){
-        return new OrderDAOImpl();
-    }
-
-    public OrderDetailDAO getOrderDetailDAO(){
-        return new OrderDetailDAOImpl();
-    }
+//    public ItemDAO getItemDAO(){
+//        return new ItemDAOImpl();
+//    }
+//
+//    public OrderDAO getOrderDAO(){
+//        return new OrderDAOImpl();
+//    }
+//
+//    public OrderDetailDAO getOrderDetailDAO(){
+//        return new OrderDetailDAOImpl();
+//    }
 }
